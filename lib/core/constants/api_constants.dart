5b8d8ac001e688f80c8b4334aae694ee9a9c229a
akfaqih3 <akfaqih3@gmail.com>
2025-05-08 16:09:04 -0700

    store details page

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConst {
-  static const String domain = "http://192.168.0.40:8000";
-  // static const String domain = "http://yemeni-offers.com";
+  // static const String domain = "http://192.168.0.40:8000";
+  static const String domain = "http://yemeni-offers.com";
   static const String baseUrl = "$domain/api/";
   static const int connectTimeout = 5000;
   static const int receiveTimeout = 5000;
