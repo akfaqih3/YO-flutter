b10e78dc424382002c1fb8cda53bdb97eecb0c9d
akfaqih3 <akfaqih3@gmail.com>
2025-05-10 10:20:49 -0700

    join as merchant featur

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConst {
-  static const String domain = "http://10.0.2.2:8000";
+  // static const String domain = "http://10.0.2.2:8000";
   // static const String domain = "http://192.168.0.40:8000";
-  // static const String domain = "http://yemeni-offers.com";
+  static const String domain = "http://yemeni-offers.com";
   static const String baseUrl = "$domain/api/";
   static const int connectTimeout = 5000;
   static const int receiveTimeout = 5000;
